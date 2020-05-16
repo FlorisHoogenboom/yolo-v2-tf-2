c762ba7452b25f4357acfb988f73c32fffa892fc
Floris Hoogenboom <floris@digitaldreamworks.nl>
2020-05-16 22:40:34 +0200

    Make imports match Google Styleguide

--- keras_yolo/layers.py
+++ keras_yolo/layers.py
@@ -1,18 +1,18 @@
-from tensorflow.keras.layers import BatchNormalization, Conv2D, LeakyReLU
-from tensorflow.keras.models import Model
+from tensorflow.keras import layers
+from tensorflow.keras import models
 
 
-class YoloConvLayer(Model):
+class YoloConvLayer(models.Model):
     def __init__(self, name, n_filters, filter_size=(3, 3), strides=(1, 1)):
         super().__init__(name=name)
 
         # Initialize the layers for this sublayer
-        self.conv = Conv2D(
+        self.conv = layers.Conv2D(
             n_filters, filter_size, strides=strides,
             padding='same', use_bias=False
         )
-        self.norm = BatchNormalization()
-        self.activation = LeakyReLU(alpha=0.1)
+        self.norm = layers.BatchNormalization()
+        self.activation = layers.LeakyReLU(alpha=0.1)
 
     def call(self, x, training=False):
         x = self.conv(x)
