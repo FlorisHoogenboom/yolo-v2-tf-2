0b1367d953e15f6c61e9048003f9d9236eb31b6a
Floris Hoogenboom <floris@digitaldreamworks.nl>
2020-05-17 18:18:49 +0200

    Implement boxes predictions.

--- keras_yolo/layers.py
+++ keras_yolo/layers.py
@@ -1,3 +1,4 @@
+import tensorflow as tf
 from tensorflow.keras import layers
 from tensorflow.keras import models
 
@@ -23,4 +24,90 @@ class YoloConvLayer(models.Model):
 
 
 class AnchorLayer(models.Model):
-    pass
+    def __init__(self,
+                 grid_height,
+                 grid_width,
+                 anchors,
+                 n_classes,
+                 *args,
+                 **kwargs):
+        super().__init__(*args, **kwargs)
+        self.grid_height = grid_height
+        self.grid_width = grid_width
+        self.anchors = anchors
+        self.n_classes = n_classes
+
+        self.boxes_conv = layers.Conv2D(
+            len(self.anchors) * 4,
+            (1, 1),
+            strides=(1, 1),
+            padding='same',
+            name='boxes',
+            activation='linear',
+        )
+
+        self.confidence_conv = layers.Conv2D(
+            len(self.anchors) * 1,
+            (1, 1),
+            strides=(1, 1),
+            padding='same',
+            name='conf',
+            activation='sigmoid',
+        )
+
+        self.classes_conv = layers.Conv2D(
+            self.n_classes,
+            (1, 1),
+            strides=(1, 1),
+            padding='same',
+            name='probas',
+            activation='softmax'
+        )
+
+    def compute_predicted_boxes(self, input):
+        unaligned_preds = self.boxes_conv(input)
+        preds = tf.reshape(
+            unaligned_preds,
+            (-1, self.grid_height, self.grid_width, len(self.anchors), 4)
+        )
+
+        yx_base = self.base_anchor_boxes[..., 0:2]
+        hw_base = self.base_anchor_boxes[..., 2:4]
+
+        # Substract 1/2 from the result of applying sigmoid to get a
+        # value in the range (-0.5, 0.5). Hence, they are fluctuating around
+        # the grid centers
+        yx = yx_base + (tf.sigmoid(preds[..., 0:2]) - 0.5)
+
+        # Clip the WH values at an arbitrary boundary such that we prevent
+        # them from exploding to infinity during training
+        # TODO: Fix the arbitrary clipping bound
+        hw = tf.clip_by_value(hw_base * tf.exp(preds[..., 2:4]), 0, 130)
+
+        return tf.concat([yx, hw], axis=-1)
+
+    @property
+    def base_anchor_boxes(self):
+        h, w = self.grid_height, self.grid_width
+
+        y_centroids = tf.tile(
+            tf.range(h, dtype='float')[:, None],
+            [1, w]
+        ) + 0.5
+        x_centroids = tf.tile(
+            tf.range(w, dtype='float')[None, :],
+            [h, 1]
+        ) + 0.5
+
+        yx_centroids = tf.concat(
+            [x_centroids[..., None, None], y_centroids[..., None, None]],
+            axis=-1
+        )
+        yx_centroids = tf.tile(yx_centroids, [1, 1, len(self.anchors), 1])
+
+        hw = tf.tile(
+            tf.constant(self.anchors)[None, None, ...],
+            [h, w, 1, 1]
+        )
+
+        return tf.concat([yx_centroids, hw], axis=-1)
--- test/test_layers.py
+++ test/test_layers.py
@@ -1,2 +1,87 @@
-def test_anchor_layer_has_correct_dimensions():
-    assert True
+import numpy as np
+
+from keras_yolo import layers
+
+
+def test_anchor_layer_base_anchor_boxes():
+    anchors = [
+        [1, 1],
+        [1, 1.5],
+        [1.5, 1]
+    ]
+    grid_height = 2
+    grid_width = 3
+    layer = layers.AnchorLayer(
+        grid_height=grid_height,
+        grid_width=grid_width,
+        anchors=anchors,
+        n_classes=7
+    )
+
+    # Check that the shape matches the anchor boxes and grid height/width
+    assert (
+        layer.base_anchor_boxes.shape ==
+        (grid_height, grid_width, len(anchors), 4)
+    )
+
+    # The xy coordinates should be centroids (hence end in 0.5)
+    assert np.all(layer.base_anchor_boxes[..., 0:2] % 1 == 0.5)
+
+    # Elements on the diagonals should be increasing stepwise
+    diag_diffs = (
+        layer.base_anchor_boxes[1:, 1:, :, 0:2] -
+        layer.base_anchor_boxes[:-1, :-1, :, 0:2]
+    )
+    assert np.all(diag_diffs == 1)
+
+    # The WH part of every coordinate section should simply match the anchors
+    assert np.all(
+        layer.base_anchor_boxes[..., 2:] == anchors
+    )
+
+
+def test_anchor_layer_predicted_boxes():
+    anchors = [
+        [1, 1],
+        [1, 1.5],
+        [1.5, 1]
+    ]
+    grid_height = 2
+    grid_width = 3
+    layer = layers.AnchorLayer(
+        grid_height=grid_height,
+        grid_width=grid_width,
+        anchors=anchors,
+        n_classes=7
+    )
+
+    input = np.random.randn(5, grid_height, grid_width, 1024)
+    output = layer.compute_predicted_boxes(input)
+
+    assert output.shape == (5, grid_height, grid_width, len(anchors), 4)
+
+    # Since we are working with coordinates on the grid all the predictions
+    # should be positive
+    assert np.all(output >= 0)
+
+    # The differences between adjacent cells should be max 2 since that is the
+    # maximum grid size
+    diag_diffs = (
+        output[:, 1:, 1:, :, 0:2] - output[:, :-1, :-1, :, 0:2]
+    )
+    assert np.all((0 <= diag_diffs) & (diag_diffs <= 2))
+
+    input = np.zeros(((5, grid_height, grid_width, 1024)))
+    output = layer.compute_predicted_boxes(input)
+
+    # With no stimulus, all predicted coordinates should be equal to the base
+    # anchor boxes
+    assert np.all((output - layer.base_anchor_boxes) == 0)
+
+
+def test_anchor_layer_dimensions():
+    pass
+
+
+def test_anchor_layer_fed_with_zeros():
+    pass
